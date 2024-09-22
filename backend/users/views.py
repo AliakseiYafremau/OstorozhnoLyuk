7749b6f783f8f68a46a882324449d20ac63d18ad
Aliaksei Yafremau <447654972g@gmail.com>
2024-09-22 22:36:18 +0200

    Upgrade Swagger Schema

--- backend/users/views.py
+++ backend/users/views.py
@@ -8,6 +8,9 @@ from .models import User
 from .permissions import IsAdmin, IsModerator
 from .serializers import LoginSerializer, UserSerializer, CreateUserSerializer
 from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
+
+from .simple_jwt_serializers import TokenObtainPairResponseSerializer
 
 
 class UserView(APIView):
@@ -17,15 +20,35 @@ class UserView(APIView):
             return [permissions.IsAuthenticated()]
         return [permissions.IsAuthenticated(), IsAdmin()]
 
+    @swagger_auto_schema(
+        operation_description='Получение информации о пользователе. Необходимо быть авторизованным. Никакие параметры не нужны.',
+        responses={
+            200: UserSerializer,
+            401: "Некорректные данные"
+        },
+    )
     def get(self, request):
         user = User.objects.get(id=request.user.id)
         user_serializer = UserSerializer(user)
         return Response(user_serializer.data, status=status.HTTP_200_OK)
     
-    # @swagger_auto_schema(
-    #     request_body=CreateUserSerializer,
-    #     responses={200: 'Success'}
-    # )
+    @swagger_auto_schema(
+        operation_description='Создание нового пользователя. Необходимо быть администратором.',
+        request_body=openapi.Schema(
+            required=['email', 'password', 'is_moderator', 'is_admin'],
+            type=openapi.TYPE_OBJECT,
+            properties={
+                'email': openapi.Schema(type=openapi.TYPE_STRING, description='Почта пользователя'),
+                'password': openapi.Schema(type=openapi.TYPE_STRING, description='Пароль пользователя'),
+                'is_moderator': openapi.Schema(type=openapi.TYPE_BOOLEAN, description='Модератор или нет'),
+                'is_admin': openapi.Schema(type=openapi.TYPE_BOOLEAN, description='Админ или нет'),
+            }
+        ),
+        responses={
+            201: UserSerializer,
+            400: "Некорректные данные"
+        }
+    )
     def post(self, request):
         serializer = CreateUserSerializer(data=request.data)
         if serializer.is_valid():
@@ -41,6 +64,22 @@ class UserView(APIView):
 
 
 class LoginView(APIView):
+
+    @swagger_auto_schema(
+        operation_description='Аутентификация пользователя.',
+        request_body=openapi.Schema(
+            required=['email', 'password', 'is_moderator', 'is_admin'],
+            type=openapi.TYPE_OBJECT,
+            properties={
+                'email': openapi.Schema(type=openapi.TYPE_STRING, description='Почта пользователя'),
+                'password': openapi.Schema(type=openapi.TYPE_STRING, description='Пароль пользователя'),
+            }
+        ),
+        responses={
+            201: TokenObtainPairResponseSerializer,
+            401: "Некорректные данные"
+        }
+    )
     def post(self, request):
         serializer = LoginSerializer(data=request.data)
         if serializer.is_valid():
