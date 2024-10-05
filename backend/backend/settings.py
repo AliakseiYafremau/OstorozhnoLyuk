fb20ee8cd1edee99b28446de9b06f43b48d28d76
Aliaksei Yafremau <447654972g@gmail.com>
2024-10-05 17:29:39 +0200

    Add Debug config

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -28,7 +28,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if os.getenv('DJANGO_DEBUG') == 'True':
+    DEBUG = True
+else:
+    DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'sf-hackathon.xyz']
 
